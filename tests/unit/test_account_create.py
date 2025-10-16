a50cda47d65b1edb1c5ff0b448b8fcce4c5268d9
tomek_dylik <t.dylik.030@studms.ug.edu.pl>
2025-10-16 13:34:37 +0200

    feature3: add pesel walidation

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -9,3 +9,10 @@ class TestAccount:
         assert account.balance == 0.0
         assert account.pesel == "12345678910"
 
+    def test_pesel_too_short(self):
+        account = Account("Jane", "Doe", "12345")
+        assert account.pesel == "Inavlid"
+    
+    def test_pesel_too_long(self):
+        account = Account("Jane", "Doe", "123451234512345")
+        assert account.pesel == "Inavlid"
