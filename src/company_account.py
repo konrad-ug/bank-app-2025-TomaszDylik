105123a562550c2641517addb02494e4c8df134f
tomek_dylik <t.dylik.030@studms.ug.edu.pl>
2025-10-29 11:23:35 +0100

    feature7: add company account

---
+++ src/company_account.py
@@ -0,0 +1,12 @@
+from src.account import Account
+
+class CompanyAccount(Account):
+    def __init__(self, name, nip):
+        self.name = name
+        self.nip = nip if self.is_nip_valid(nip) else "Invalid"
+        self.balance = 0.0
+        
+    def is_nip_valid(self, nip):
+        if isinstance(nip, str) and len(nip) == 10 and nip.isdigit():
+            return True
+        return False
