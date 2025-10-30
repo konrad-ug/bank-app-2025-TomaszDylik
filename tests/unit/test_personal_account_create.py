74e1811171b8abb2ea67297173df4c23bb749659
tomek_dylik <t.dylik.030@studms.ug.edu.pl>
2025-10-30 13:53:06 +0100

    feature89b: small refactor

--- tests/unit/test_personal_account_create.py
+++ tests/unit/test_personal_account_create.py
@@ -23,9 +23,6 @@ class TestAccount:
 
     # tests for promo_code 
     # valid
-    def test_promo_code_invalid_not_is_instance_str(self):
-        account = PersonalAccount("John", "Doe", "12345678910", promo_code=bool(True))
-        assert account.balance == 0.0
     def test_promo_code_valid_format_adds_50(self):
         account = PersonalAccount("John", "Doe", "12345678910", promo_code="PROM_ABC")
         assert account.balance == 50
@@ -38,6 +35,9 @@ class TestAccount:
         assert account.balance == 50.0
 
     # none-valid
+    def test_promo_code_invalid_not_is_instance_str(self):
+        account = PersonalAccount("John", "Doe", "12345678910", promo_code=bool(True))
+        assert account.balance == 0.0
     def test_promo_code_none_no_bonus(self):
         account = PersonalAccount("John", "Doe", "12345678910", promo_code=None)
         assert account.balance == 0.0
